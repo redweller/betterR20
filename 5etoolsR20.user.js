bc5f39ecfc244e3548e45a62b757fb947855d3f5
Miles Long <TheGiddyLimit@users.noreply.github.com>
2017-12-06 15:28:09 +0000

    chore(repo) switch repo

--- 5etoolsR20.user.js
+++ 5etoolsR20.user.js
@@ -2,9 +2,9 @@
 // @name         5etoolsR20
 // @namespace    https://github.com/astranauta/
 // @license      MIT (https://opensource.org/licenses/MIT)
-// @version      0.5.25
-// @updateURL    https://github.com/astranauta/5etoolsR20/raw/master/5etoolsR20.user.js
-// @downloadURL  https://github.com/astranauta/5etoolsR20/raw/master/5etoolsR20.user.js
+// @version      0.5.26
+// @updateURL    https://github.com/TheGiddyLimit/5etoolsR20/raw/master/5etoolsR20.user.js
+// @downloadURL  https://github.com/TheGiddyLimit/5etoolsR20/raw/master/5etoolsR20.user.js
 // @description  Enhance your Roll20 experience
 // @author       5egmegaanon/astranauta/MrLabRat/TheGiddyLimit/DBAWiseMan
 // @match        https://app.roll20.net/editor/
@@ -16,11 +16,11 @@
 
 var D20plus = function(version) {
 
-	var monsterdataurl = "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/data/bestiary.json";
-	var monsterdataurlTob = "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/data/bestiary-tob.json";
-	var spelldataurl = "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/data/spells.json";
-	var spellmetaurl = "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/data/spells-roll20.json";
-	var itemdataurl = "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/data/items.json";
+	var monsterdataurl = "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/data/bestiary.json";
+	var monsterdataurlTob = "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/data/bestiary-tob.json";
+	var spelldataurl = "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/data/spells.json";
+	var spellmetaurl = "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/data/spells-roll20.json";
+	var itemdataurl = "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/data/items.json";
 
 	var d20plus = {
 		sheet: "ogl",
@@ -37,8 +37,8 @@ var D20plus = function(version) {
 	d20plus.scripts = [
 		{name: "xml2json", url: "https://cdnjs.cloudflare.com/ajax/libs/x2js/1.2.0/xml2json.min.js"},
 		{name: "listjs", url: "https://raw.githubusercontent.com/javve/list.js/v1.5.0/dist/list.min.js"},
-		{name: "5etoolsutils", url: "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/js/utils.js"},
-		{name: "5etoolsrender", url: "https://raw.githubusercontent.com/astranauta/astranauta.github.io/master/js/entryrender.js"}
+		{name: "5etoolsutils", url: "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/js/utils.js"},
+		{name: "5etoolsrender", url: "https://raw.githubusercontent.com/TheGiddyLimit/TheGiddyLimit.github.io/master/js/entryrender.js"}
 	];
 
 	// Inject external JS libraries
