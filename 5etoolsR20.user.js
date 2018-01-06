0d7b4366ebe3b6bcee0ac153f42a7642492a2465
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-01-06 13:01:57 +0000

    chore(script) bump version number

--- 5etoolsR20.user.js
+++ 5etoolsR20.user.js
@@ -2,7 +2,7 @@
 // @name         5etoolsR20
 // @namespace    https://rem.uz/
 // @license      MIT (https://opensource.org/licenses/MIT)
-// @version      0.5.42
+// @version      0.6.0
 // @updateURL    https://get.5etools.com/5etoolsR20.user.js
 // @downloadURL  https://get.5etools.com/5etoolsR20.user.js
 // @description  Enhance your Roll20 experience
