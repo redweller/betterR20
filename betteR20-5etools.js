15287078b02e286be6f8791efebdff36f3d10331
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-03-28 19:59:59 +0100

    fix(importer): revert token suffix changes; add "on import" note

--- betteR20-5etools.js
+++ betteR20-5etools.js
@@ -129,7 +129,7 @@ const betteR205etools = function () {
 			"_type": "_DAMAGEMODE"
 		},
 		"namesuffix": {
-			"name": "Append Text to Names",
+			"name": "Append Text to Names on Import",
 			"default": "",
 			"_type": "String"
 		}
@@ -364,16 +364,6 @@ const betteR205etools = function () {
 						});
 						var isNPC = npc ? parseInt(npc.get("current")) : 0;
 						if (isNPC) {
-							// Append text if configured to do so
-							const nameSuffix = d20plus.getCfgVal("token", "namesuffix");
-							if (nameSuffix && nameSuffix.trim()) {
-								setTimeout(() => {
-									if (e.attributes["name"]) {
-										e.attributes["name"] = e.attributes["name"] + " " + nameSuffix;
-									}
-								}, 750);
-							}
-
 							// Set bars if configured to do so
 							var barsList = ["bar1", "bar2", "bar3"];
 							$.each(barsList, (i, barName) => {
@@ -1534,9 +1524,10 @@ const betteR205etools = function () {
 		if (character.senses && character.senses.toLowerCase().match(/(darkvision|blindsight|tremorsense|truesight)/)) lightradius = Math.max.apply(Math, character.senses.match(/\d+/g));
 		var lightmin = 0;
 		if (character.senses && character.senses.toLowerCase().match(/(blindsight|tremorsense|truesight)/)) lightmin = lightradius;
+		const nameSuffix = d20plus.getCfgVal("token", "namesuffix");
 		var defaulttoken = {
 			represents: character.id,
-			name: character.name,
+			name: `${character.name}${nameSuffix ? ` ${nameSuffix}` : ""}`,
 			imgsrc: avatar,
 			width: 70 * tokensize,
 			height: 70 * tokensize,
