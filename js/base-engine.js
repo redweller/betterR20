8f2b0eb648c83c995d8b121f1d110bd53a761868
TheGiddyLimit <TheGiddyLimit@5etools.com>
2018-12-03 20:52:51 +0000

    fix: maintain other statuses when setting flight height

--- js/base-engine.js
+++ js/base-engine.js
@@ -833,9 +833,15 @@ function d20plusEngine () {
 									]
 								});
 							}).then(num => {
-								const statusString = `${num}`.split("").reverse().map(it => `fluffy-wing@${it}`).join(",");
+								const STATUS_PREFIX = `fluffy-wing@`;
+								const statusString = `${num}`.split("").reverse().map(it => `${STATUS_PREFIX}${it}`).join(",");
 								sel.forEach(s => {
-									s.model.set("statusmarkers", statusString);
+									const existing = s.model.get("statusmarkers");
+									if (existing && existing.trim()) {
+										s.model.set("statusmarkers", [statusString, ...existing.split(",").filter(it => it && it && !it.startsWith(STATUS_PREFIX))].join(","));
+									} else {
+										s.model.set("statusmarkers", statusString);
+									}
 									s.model.save();
 								});
 							});
