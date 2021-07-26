8e92f26b1304a6a4df6f215d75dfa456ddda72b6
Mertang <mertang92@gmail.com>
2021-07-26 03:24:54 +0300

    added TODO comments

--- js/5etools-main.js
+++ js/5etools-main.js
@@ -2814,6 +2814,7 @@ const betteR205etoolsMain = function () {
 				const toImport = MiscUtil.copy(data.race);
 				if (data.subrace){
 					const allraces = await DataUtil.loadJSON(RACE_DATA_URL);
+					//TODO maybe load homebrew parent race if required from the subraces.
 					toImport.push(...d20plus.races.adoptSubraces(allraces.race, data.subrace, false))
 				}
 				d20plus.importer.addBrewMeta(data._meta);
