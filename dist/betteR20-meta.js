c40cd220995e9f4a8d561886dea6f49a04c7f168
oldewyrm <79656029+oldewyrm@users.noreply.github.com>
2022-01-07 22:13:27 -0700

    Update betteR20-meta.js

--- dist/betteR20-meta.js
+++ dist/betteR20-meta.js
@@ -2,7 +2,8 @@
 // @name         betteR20-meta
 // @namespace    https://5e.tools/
 // @license      MIT (https://opensource.org/licenses/MIT)
-// @version      1.28.1.1-dev
+// @version      1.28.2
+// @updateURL    https://raw.githubusercontent.com/oldewyrm/betterR20/auto-update/dist/betteR20-meta.js
 // @description  Enhance your Roll20 experience
 // @author       TheGiddyLimit
 // @grant        unsafeWindow
