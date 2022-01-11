485d1763bc95430a56007c343d5535a05eb3941b
TheGiddyLimit <thegiddylimit@5etools.com>
2022-01-11 17:50:58 +0000

    chore: add stub changelog file
    Scraped from the GitHub API via:
    
    ```js
    fetch("https://api.github.com/repos/TheGiddyLimit/betterR20/releases")
      .then(res => res.json()).then(arr => {
      LOGS = arr.map(it => ({
          ver: it.tag_name.slice(1),
          date: it.created_at.slice(0, 10),
          txt: it.body,
        })).reverse()
      })
    copy(LOGS)
    ```

--- node/get-data.js
+++ node/get-data.js
@@ -7,11 +7,14 @@ if (!process.argv[2]) {
 	process.exit(1);
 }
 
+const _BLACKLIST_FILENAMES_JSON = new Set([
+	"changelog.json",
+]);
+
 // region https://stackoverflow.com/a/55566081
 
 const recursiveReadDir = (p, a = []) => {
-	if (fs.statSync(p).isDirectory())
-		fs.readdirSync(p).map(f => recursiveReadDir(a[a.push(path.join(p, f)) - 1], a))
+	if (fs.statSync(p).isDirectory()) fs.readdirSync(p).map(f => recursiveReadDir(a[a.push(path.join(p, f)) - 1], a))
 	return a
 }
 // endregion
@@ -21,6 +24,7 @@ async function main () {
 
 	for (const pth of curListing) {
 		if (!pth.endsWith(".json")) continue;
+		if (_BLACKLIST_FILENAMES_JSON.has(path.basename(pth))) continue;
 		const pathSiteDir = path.join(process.argv[2], pth);
 		if (!fs.existsSync(pathSiteDir)) throw new Error(`File ${pth} does not exist in 5etools data!`);
 		fs.copyFileSync(pathSiteDir, pth);
