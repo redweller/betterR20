b080b7cac0367320cd5fa8f7f46999f10cceed24
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-04-21 10:13:06 +0100

    chore: log changes; bump version

--- dist/betteR20-core.meta.js
+++ dist/betteR20-core.meta.js
@@ -2,7 +2,7 @@
 // @name         betteR20-core
 // @namespace    https://5e.tools/
 // @license      MIT (https://opensource.org/licenses/MIT)
-// @version      1.30.1
+// @version      1.30.2
 // @updateURL    https://github.com/TheGiddyLimit/betterR20/raw/development/dist/betteR20-core.meta.js
 // @downloadURL  https://github.com/TheGiddyLimit/betterR20/raw/development/dist/betteR20-core.user.js
 // @description  Enhance your Roll20 experience
@@ -21,5 +21,6 @@
 // @grant        GM_webRequest
 // @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
 // @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
 
 // ==/UserScript==
