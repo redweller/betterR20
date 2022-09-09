598d54f1e45057948f442f4d15d7b8c5bc2cd2ad
Notanyone <redweller@gmail.com>
2022-09-09 23:35:22 +0300

    Bump version

--- node/build-scripts.js
+++ node/build-scripts.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const SCRIPT_VERSION = "1.31.0";
+const SCRIPT_VERSION = "1.31.0.18";
 const SCRIPT_REPO = "https://github.com/redweller/betterR20/raw/run/"
 
 const matchString = `
