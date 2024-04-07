eb15b1dc68873ac09112e6b4bb60cfdec2ed1df5
Redweller <redweller@gmail.com>
2024-04-07 21:34:50 +0300

    5etools alpha

---
+++ alpha/betteR20-5etools.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         betteR20-alpha-5etools
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.35.185a
+// @updateURL    https://github.com/redweller/betterR20/raw/beta/alpha/betteR20-5etools.meta.js
+// @downloadURL  https://github.com/redweller/betterR20/raw/beta/alpha/betteR20-5etools.user.js
+// @description  Enhance your Roll20 experience
+// @author       5egmegaanon/astranauta/MrLabRat/TheGiddyLimit/DBAWiseMan/BDeveau/Remuz/Callador Julaan/Erogroth/Stormy/FlayedOne/Cucucc/Cee/oldewyrm/darthbeep/Mertang/Redweller
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
---
+++ alpha/betteR20-core.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         betteR20-alpha-core
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.35.185a
+// @updateURL    https://github.com/redweller/betterR20/raw/beta/alpha/betteR20-core.meta.js
+// @downloadURL  https://github.com/redweller/betterR20/raw/beta/alpha/betteR20-core.user.js
+// @description  Enhance your Roll20 experience
+// @author       TheGiddyLimit
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
